351f706fe27db47d0d6fe37d728d414b9bb8d330
Muhammed Mamunur Rashid <96053862+mamunurrashid1010@users.noreply.github.com>
2023-08-14 15:11:25 +0600

    category route added.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('category',App\Http\Controllers\CategoryController::class)->only(['index','store','show','update','destroy']);
