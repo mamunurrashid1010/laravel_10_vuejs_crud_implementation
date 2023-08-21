cf4bf22fc1b102446a06ad87bc085430dc858133
Muhammed Mamunur Rashid <96053862+mamunurrashid1010@users.noreply.github.com>
2023-08-21 15:10:26 +0600

    Create components directory and App.vue, Welcome.vue component implement

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>Welcome</h1>
+            <a href="#" target="_blank">Visit For More Awesome Articles</a>
+        </div>
+    </div>
+</template>
