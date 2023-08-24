8ee2fee2659b12110a79e433894c20f59abee62d
Muhammed Mamunur Rashid <96053862+mamunurrashid1010@users.noreply.github.com>
2023-08-24 15:09:22 +0600

    Category list implement

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,10 +1,16 @@
 const Welcome = () => import('./components/Welcome.vue' /* webpackChunkName: "resource/js/components/welcome" */)
+const CategoryList = () => import('./components/category/List.vue' /* webpackChunkName: "resource/js/components/category/list" */)
 
 export const routes = [
     {
         name: 'home',
         path: '/',
         component: Welcome
+    },
+    {
+        name: 'categoryList',
+        path: '/category',
+        component: CategoryList
     },
 
 ]
